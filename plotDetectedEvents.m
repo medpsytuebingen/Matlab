ddecd8aab399eb2e406219b78cb33d438a7d51ae
Jens K <jens-k@users.noreply.github.com>
2020-09-09 00:19:25 -0400

    Add plotting of spectrum

--- plotDetectedEvents.m
+++ plotDetectedEvents.m
@@ -1,6 +1,12 @@
 function plotDetectedEvents(output, path_plot)
-% Plots a summary (scoring, length of data, detected slow waves and
-% spindles) of the results returned by detectEvents.
+% Plots a summary of the results returned by detectEvents. This includes
+% the power spectrum, sleep scoring, length of data, detected slow waves
+% and spindles, their phase coupling etc., depending on what is present in
+% the data. 
+%
+% Note: This was mainly tested using 4 channels. Placing several plots on
+% top of each other is tricky, so every help is needed to make this more
+% robust for other numnbers of channels.
 %
 % INPUT VARIABLES:
 % output						Output of detectEvents
@@ -12,9 +18,6 @@ function plotDetectedEvents(output, path_plot)
 %
 % AUTHOR:
 % Jens Klinzing, klinzing@princeton.edu
-
-%% TODO
-% . currently only plots SOs and spindles
 
 %% INITIALIZATION AND SETTINGS
 Fs							= output.info.Fs;			% sample rate of the artifact definition
@@ -117,15 +120,24 @@ cmap.REM                 = [0 .5 0];
 num_col		= 6; % flexibly usable columns
 num_pnl		= 2; % event and spectrum panel
 
-%% PLOT EVENT PANELS
+%% PLOT EVENT PANEL
 pnl			= 1; % first panel
-splts		= 1:num_chans*num_col*pnl;
-splts_l		= sort([num_col*[0:num_chans-1]+1 num_col*[0:num_chans-1]+2]); % all subplot indices for the left side
-splts_r		= splts(~ismember(splts, splts_l)); % and right side
+splts		= 1:num_chans*pnl*num_col;
+splts_r		= sort([num_col*[0:num_chans-1]+num_col-1 num_col*[0:num_chans-1]+num_col]);
+splts_l		= splts(~ismember(splts, splts_r)); % and right side
 
 h = figure;
-set(h,'Position',[50 100 1400 num_chans*140]);
-subplot(num_chans,num_col,splts_r)
+% set(h,'Position',[20 100 1400 num_chans*130+500]);
+set(h, 'Position', get(0, 'Screensize')); % fullscreen
+
+% Left: Scoring and event distribution
+subplot(num_chans*num_pnl,num_col,splts_l)
+if isfield(output.info, 'name')
+	title(['Dataset: ' output.info.name ', recording length: ' num2str(output.info.length / output.info.Fs/60) ' min'])
+else
+	title(['Recording length: ' num2str(output.info.length / output.info.Fs/60) ' min'])
+end 
+
 hold on
 sleepStage_legend        = cell(0);  % for the legend, collect names of sleep stages that have actually occured
 handlevector = [];
@@ -136,7 +148,7 @@ for iSt = sleepStage' % Needs ' for whatever reason
 	ep_starts			= strfind(t_diffs,[0 1]); % where does scoring flip to this stage
 	ep_ends				= strfind(t_diffs,[1 0]);
 	for iEp = 1:length(ep_starts)
-		temphandle			= line([t(ep_starts(iEp))/60 t(ep_ends(iEp))/60],[lineHeight_hyp lineHeight_hyp], 'LineWidth', 20, 'Color', cmap.(iSt{1}));
+		temphandle			= line([t(ep_starts(iEp))/60 t(ep_ends(iEp))/60],[lineHeight_hyp lineHeight_hyp], 'LineWidth', 30, 'Color', cmap.(iSt{1}));
 	end
 	handlevector			= [handlevector temphandle];
 	sleepStage_legend		= [sleepStage_legend iSt{1}]; % add handle of one of the lines
@@ -166,22 +178,22 @@ if spi
 end
 
 % Labels, title, legend
-ylim([lineHeight_hyp - (0.1*num_chans)-0.05 .1])
+ylim([lineHeight_hyp - (0.1*num_chans)-0.1 .2])
 xlabel('Time (in min)')
 xlim([0 inf])
 set(gca,'YTick', [])
+set(gca,'YTickLabel', [])
 ylabel('Channels (in the order provided)');
-sgtitle(sprintf('Quality check'),  'FontSize', 14);
 if slo && spi
-	leg_str = {'Slow oscillations/waves', 'Spindles'};
+	leg_str = {'SOs', 'Spindles'};
 elseif slo
-	leg_str = {'Slow oscillations/waves'};
+	leg_str = {'SOs'};
 elseif spi
 	leg_str = {'Spindles'};
 else
 	leg_str = {};
 end
-legend(handlevector(:), 'Recording Length', sleepStage_legend{:}, leg_str{:}, 'Location', 'EastOutside')
+legend(handlevector(:), 'Rec length', sleepStage_legend{:}, leg_str{:}, 'Location', 'SouthWest')
 
 if slo
 	% Average SOs
@@ -192,7 +204,7 @@ if slo
 	err_metric				= 'std';
 	win_width				= floor(size(output.slo.waveform{iCh}(1,:),2)/2); % in samples
 	for iCh = 1:num_chans
-		p = subplot(num_chans,num_col,splts_l([(iCh-1)*2+1 (iCh-1)*2+2]));
+		p = subplot(num_chans*num_pnl,num_col,splts_r([(iCh-1)*2+1 (iCh-1)*2+2]));
 		y_mean = mean(output.slo.waveform{iCh},1);
 		y_std = std(output.slo.waveform{iCh},0,1);
 		switch(err_metric)
@@ -203,6 +215,7 @@ if slo
 		end
 		x_axis = (-win_width:win_width)/Fs; % in seconds
 		x_vector = [x_axis, fliplr(x_axis)];
+		yyaxis right
 		if size(output.slo.waveform{iCh}, 2) > 1
 			patch = fill(x_vector, [y_mean+err,fliplr(y_mean-err)], color_area);
 			set(patch, 'edgecolor', 'none');
@@ -211,7 +224,10 @@ if slo
 		end
 		plot(x_axis, y_mean, 'color', color_line, 'LineWidth', line_width, 'LineStyle', '-', 'Color', 'black');
 		xlim([x_axis(1) x_axis(end)])
-		ylabel(output.info.channel{iCh}, 'Interpreter', 'none');
+		ylabel(output.info.channel{iCh}, 'Interpreter', 'none', 'Color', 'black');
+		ax = gca;
+		ax.YAxis(1).Visible = 0; % hide left unused axis
+		ax.YAxis(2).Color = 'k'; % turn right axis black
 		y_lim = ylim;
 		spi_y = y_lim(1) + (y_lim(2)-y_lim(1))/3; % plot spindles at 1/3 of the ylim height
 		
@@ -230,7 +246,7 @@ if slo
 			
 			% Add polar histograms
 			if isfield(output, 'SloSpiAmpCoupling')
-				axes('pos',[p.Position(1)-p.Position(3)*.1 p.Position(2)+p.Position(4)*.2 p.Position(3)*.4 p.Position(3)*.4]) % left, bottom, width, height
+				axes('pos',[p.Position(1) p.Position(2)+p.Position(4)*.11 p.Position(3)*.2 p.Position(3)*.2]) % left, bottom, width, height
 				pol = polarhistogram(deg2rad(output.SloSpiAmpCoupling{iCh}));
 				pol.Parent.ThetaAxis.Visible = 'off';
 				pol.Parent.RAxis.Visible = 'off';
@@ -241,7 +257,7 @@ if slo
 				end
 			end
 			if isfield(output, 'SloSpiDetCoupling')
-				axes('pos',[p.Position(1)+p.Position(3)*.7 p.Position(2)+p.Position(4)*.2 p.Position(3)*.4 p.Position(3)*.4]) % left, bottom, width, height
+				axes('pos',[p.Position(1)+p.Position(3)*.8 p.Position(2)+p.Position(4)*.11 p.Position(3)*.2 p.Position(3)*.2]) % left (first argument) had +p.Position(3)*.7
 				pol = polarhistogram(deg2rad(output.SloSpiDetCoupling{iCh}));
 				pol.Parent.ThetaAxis.Visible = 'off';
 				pol.Parent.RAxis.Visible = 'off';
@@ -252,11 +268,45 @@ if slo
 				end
 			end
 		end
+		
 	end
 end
 drawnow
 
-% Save the plot
+%% PLOT SPECTRUM PANEL
+if spectrum
+	pnl			= 2; % first panel
+	splts		= (pnl-1)*(num_chans*num_col)+1:num_chans*num_col*pnl; % this subplot business is getting confusing, are there better ways to flexibly manage subplots?	
+	splts_l = splts(logical(mod(ceil((1:length(splts)) / (num_col/2)), 2)));
+	splts_r = splts(~logical(mod(ceil((1:length(splts)) / (num_col/2)), 2)));
+	
+	% Left: NREM
+	if isfield(output.spectrum, 'rel_nrem')
+		s1 = subplot(num_chans*num_pnl,num_col,splts_l);
+		plot(output.spectrum.freq, output.spectrum.rel_nrem, '-', 'LineWidth', 2)
+		ylim([-1 max(max(output.spectrum.rel_nrem(:, output.spectrum.freq > 2 & output.spectrum.freq < 30))) * 2])
+		ylabel('Oscillatory relative to fractal component')
+		title(sprintf('\n Spectrum NREM sleep'))
+		legend(output.info.channel{:})
+		xlim([1 35]) % thats where the really interesting stuff happens
+	end
+	
+	% Right: REM
+	if isfield(output.spectrum, 'rel_rem')
+		s2 = subplot(num_chans*pnl,num_col,splts_r);
+		plot(output.spectrum.freq, output.spectrum.rel_rem, '-', 'LineWidth', 2)
+		ylim([-1 max(max(output.spectrum.rel_rem(:, output.spectrum.freq > 2 & output.spectrum.freq < 30))) * 2])
+		ylabel('Oscillatory relative to fractal component')
+		legend(output.info.channel{:})
+		title(sprintf('\n Spectrum REM sleep'))
+	end
+end
+
+% Shift both subplots a but down to make everything look better
+s1.Position = [s1.Position(1) s1.Position(2)-.04 s1.Position(3) s1.Position(4)];
+s2.Position = [s2.Position(1) s2.Position(2)-.04 s1.Position(3) s2.Position(4)];
+
+%% Save the plot
 if nargin > 1 && ~isempty(path_plot)
 	[~,name,ext] = fileparts(path_plot);
 	saveas(gcf,path_plot)
