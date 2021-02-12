5bb43be749bbcaab4726b12650e252b44be1455b
NNiethard <70136211+NNiethard@users.noreply.github.com>
2021-02-12 16:13:51 +0100

    isArt Update
    include case without defined artifact.

--- event_detector/detectEvents.m
+++ event_detector/detectEvents.m
@@ -1112,14 +1112,18 @@ end
 
 %% Local functions
 function art = isArt(evt, arts)
-% Checks for a provided event window ([beg end]) whether it overlap with
-% any of n provided artifact windows (n x 2).
-if length(evt) ~= 2 || size(arts, 2) ~= 2
-	error('Incorrect input format.')
+if ~isempty(arts)
+    % Checks for a provided event window ([beg end]) whether it overlap with
+    % any of n provided artifact windows (n x 2).
+    if length(evt) ~= 2 || size(arts, 2) ~= 2
+        error('Incorrect input format.')
+    end
+    
+    % Is there an artifact where the artifact end if after the event start sample  is before th
+    art = any(arrayfun(@(x) arts(x, 2) >= evt(1) && arts(x, 1) <= evt(end), 1:size(arts,1)));
+else
+    art = logical(0);
 end
-
-% Is there an artifact where the artifact end if after the event start sample  is before th
-art = any(arrayfun(@(x) arts(x, 2) >= evt(1) && arts(x, 1) <= evt(end), 1:size(arts,1)));
 end
 
 %% Local functions for generic event detection
